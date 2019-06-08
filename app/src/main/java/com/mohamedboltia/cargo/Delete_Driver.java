b86c7e47cb12080923cb024739dc1ee3800ed017
mohammedboltya <mmmwe16@yahoo.com>
2019-06-08 22:58:29 +0200

    Screens

---
+++ app/src/main/java/com/mohamedboltia/cargo/Delete_Driver.java
@@ -0,0 +1,13 @@
+package com.mohamedboltia.cargo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Delete_Driver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete__driver);
+    }
+}
---
+++ app/src/main/java/com/mohamedboltia/cargo/Nofication_Import.java
@@ -0,0 +1,13 @@
+package com.mohamedboltia.cargo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nofication_Import extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nofication__import);
+    }
+}
